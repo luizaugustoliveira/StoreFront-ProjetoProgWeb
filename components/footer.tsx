24f4efd923308d3ca4954ae10f93383e82b4b721
Luiz Augusto Oliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2025-04-09 11:47:41 -0300

    atualizações de estilo

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,11 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2023 KenDev.Co. All rights reserved.
+          &copy;All rights reserved.
         </p>
       </div>
     </footer>
-  )
+  );
 };
 
 export default Footer;
